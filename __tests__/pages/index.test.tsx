93ce2713ebea92a6ee3dcfd1ddcada84a5e78a1e
Alex <aleksandar.lokoski@gmail.com>
2023-03-01 19:13:56 -0500

    Setting up a basic app and access to contentful data

---
+++ __tests__/pages/index.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen, waitFor } from '@testing-library/react'
+// import Home from '../../src/app/page'
+// import '@testing-library/jest-dom'
+
+
+// describe('Home', () => {
+//   it('renders a heading',  () => {
+//     render( <Home />)
+
+//     const heading = screen.getByRole('heading')
+
+
+//     expect(heading).toHaveTextContent("Hello world")
+//   })
+// })
+
+//THIS TEST WONT WORK ON HOME PAGE BECAUSE WE ARE USING NEXT 13!!
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,26 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { createClient } from 'contentful';
 import styles from './page.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+var client = createClient({
+  space: process.env.CONTENTFUL_SPACE_KEY as string,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
+});
 
-export default function Home() {
+async function getContentfulData() {
+  const contentfulData = await client.getEntries({
+    content_type: "course",
+  });  
+  return contentfulData.items;
+}
+
+async function Home() {
+
+  const contentfulData = await getContentfulData();
+  //contentfulData.map((cardInfo:any )=> <CourseCardGoesHere cardInfo={cardInfo.fields}/>) // this will return two objects where you can find the info needed to create a course card
+  
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+   <h1 >Hello world</h1>
   )
 }
+
+export default Home;
