40469306883256c6be79a93c957cd7e54f71ac4f
phong1612 <dinhnhatphong1612@gmail.com>
2023-03-16 07:14:13 -0400

    Commit challenge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { createClient } from 'contentful';
-import styles from './page.module.css'
-
+import styles from "./page.module.css";
 var client = createClient({
   space: process.env.CONTENTFUL_SPACE_KEY as string,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
@@ -17,11 +16,16 @@ export default async function Home() {
 
   const contentfulData = await getContentfulData();
   //contentfulData.map((cardInfo:any )=> <CourseCardGoesHere cardInfo={cardInfo.fields}/>) // this will return two objects where you can find the info needed to create a course card
-  
   return (
-    <div>
-     <h1 >Hello Avocademy</h1>
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <div className={styles.cardheader}>Certification</div>
+        <div className={styles.cardbody}>
+          <h2>UX Design for VR</h2>
+          <p>Get to learn about the benefits of VR, the latest software and tools that the industry is using and so much more.</p>
+        </div>
+        <div className={styles.cardend}>13 hrs</div>
+      </div>
     </div>
   )
 }
-
